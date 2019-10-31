35726249b1a873f9e499dbfe7b696d8677fee9d5
Strzechowski <rafal.strzechowski1@gmail.com>
2019-10-31 08:54:59 +0100

    added proper host to ALLOWED_HOSTS

--- inzynier/settings.py
+++ inzynier/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&y4q^+t8=^ra20s5i!6#cmz(bg&#ofq)fdb#!$dv3xymck*=#^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['inzynier.inzynier.com']
+ALLOWED_HOSTS = ['inzynier.herokuapp.com']
 
 
 # Application definition
