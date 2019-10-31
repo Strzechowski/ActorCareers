0c319c4bd1449f592527d85747557e86530d71ee
Strzechowski <rafal.strzechowski1@gmail.com>
2019-10-31 09:45:04 +0100

    another css changes for production

--- inzynier/settings.py
+++ inzynier/settings.py
@@ -133,6 +133,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     "/home/strzecho/learning/inzynierka/inzynier/praca_inzynierska/static/praca_inzynierska/css/",
+    os.path.join(PROJECT_ROOT, 'praca_inzynierska/static/praca_inzynierska/css'),
 ]
 
 #  Add configuration for static files storage using whitenoise
