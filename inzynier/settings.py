17ccfc5c61ef9aa9bab4cc191f1e049f103c3821
Strzechowski <rafal.strzechowski1@gmail.com>
2019-10-31 09:09:38 +0100

    changed whitenoise variable

--- inzynier/settings.py
+++ inzynier/settings.py
@@ -145,8 +145,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Something with database
 
